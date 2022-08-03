86092522437da145ae9fbbb974ad91057df3d57a
rldnrl <devpark1002@gmail.com>
2022-08-03 13:21:24 +0900

    feat: implement sign in page

---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,31 @@
+import { InferGetServerSidePropsType } from "next";
+import { getProviders, signIn } from "next-auth/react";
+import GithubSignIn from "../../libs/ui/GithubSignIn";
+
+export default function SignIn({
+  providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const githubProvider = providers?.github;
+  console.log(githubProvider);
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="border rounded px-12 py-8 border-gray-600">
+        <GithubSignIn
+          onClick={() =>
+            signIn(githubProvider?.id, {
+              callbackUrl: "/",
+            })
+          }
+        />
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+};
