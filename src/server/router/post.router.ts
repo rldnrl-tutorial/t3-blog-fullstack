d3db28305a0933225ba4b185b95ba500dd00dbdf
rldnrl <devpark1002@gmail.com>
2022-08-05 16:55:30 +0900

    feat: add post router

---
+++ src/server/router/post.router.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { createRouter } from "./context";
+
+export const postProtectedRouter = createRouter()
+  .mutation("create", {
+    input: z.object({
+      title: z.string(),
+      content: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      if (!ctx.session || !ctx.session.user || !ctx.session.user.email) return;
+      await ctx.prisma.post.create({
+        data: {
+          ...input,
+          author: {
+            connect: {
+              email: ctx.session.user.email,
+            },
+          },
+        },
+      });
+    },
+  })
+  .query("getDrafts", {
+    async resolve({ ctx }) {
+      if (!ctx.session || !ctx.session.user || !ctx.session.user.email) return;
+      return await ctx.prisma.post.findMany({
+        where: {
+          author: {
+            email: ctx.session.user.email,
+          },
+        },
+        include: {
+          author: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+    },
+  });
+
+export const postRouter = createRouter().query("getAllPosts", {
+  async resolve({ ctx }) {
+    return await ctx.prisma.post.findMany();
+  },
+});
