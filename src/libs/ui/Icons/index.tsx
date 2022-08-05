25b4c08287a65c22cb35234974de8cc6661795ee
rldnrl <devpark1002@gmail.com>
2022-08-05 11:34:52 +0900

    feat: add icon

--- src/libs/ui/Icons/index.tsx
+++ src/libs/ui/Icons/index.tsx
@@ -3,3 +3,5 @@ export { default as PlusIcon } from "./PlusIcon";
 export { default as CopyIcon } from "./CopyIcon";
 export { default as LockIcon } from "./LockIcon";
 export { default as UnlockIcon } from "./UnlockIcon";
+export { default as CheckCircleIcon } from "./CheckCircleIcon";
+export { default as CloseIcon } from "./ChatIcon";
