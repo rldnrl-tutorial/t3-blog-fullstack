4a8ccf11a85af1288f1e6f7c496b87af7c62c39f
rldnrl <devpark1002@gmail.com>
2022-08-05 16:55:20 +0900

    chore: remove comment

--- src/server/router/protected-router.ts
+++ src/server/router/protected-router.ts
@@ -1,9 +1,6 @@
 import * as trpc from "@trpc/server";
 import { createRouter } from "./context";
 
-/**
- * Creates a tRPC router that asserts all queries and mutations are from an authorized user. Will throw an unauthorized error if a user is not signed in.
- */
 export function createProtectedRouter() {
   return createRouter().middleware(({ ctx, next }) => {
     if (!ctx.session || !ctx.session.user) {
@@ -12,7 +9,6 @@ export function createProtectedRouter() {
     return next({
       ctx: {
         ...ctx,
-        // infers that `session` is non-nullable to downstream resolvers
         session: { ...ctx.session, user: ctx.session.user },
       },
     });
