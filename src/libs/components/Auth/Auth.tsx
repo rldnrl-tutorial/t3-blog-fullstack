002a37f6ecf4c5c1d99c73396b739db09be4497c
rldnrl <devpark1002@gmail.com>
2022-08-07 17:09:57 +0900

    feat: implement auth component

---
+++ src/libs/components/Auth/Auth.tsx
@@ -0,0 +1,26 @@
+import { useSession } from "next-auth/react";
+import Layout from "../../ui/Layout";
+
+type AuthProps = {
+  fallback: JSX.Element;
+  failure: JSX.Element;
+  children: JSX.Element;
+};
+
+export default function Auth({ fallback, failure, children }: AuthProps) {
+  const { data: session, status } = useSession({
+    required: true,
+  });
+
+  const isUnauthorized = session === null;
+
+  if (status === "loading") {
+    return <Layout>{fallback}</Layout>;
+  }
+
+  if (isUnauthorized) {
+    return <Layout>{failure}</Layout>;
+  }
+
+  return <Layout>{children}</Layout>;
+}
